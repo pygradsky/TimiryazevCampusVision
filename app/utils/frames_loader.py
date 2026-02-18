630c1ae0d2fd64df928e028b3c045d386bdbcb46
Fedor <feda.vert@gmail.com>
2026-02-18 09:50:45 +0300

    Изменена логика выполнения функции - теперь сохраняется каждый 5 кадр. Добавил возвращение количества кадров из функции, был изменён абсолютный путь к главной папке - BASE_DIR

--- app/utils/frames_loader.py
+++ app/utils/frames_loader.py
@@ -1,12 +1,14 @@
 import cv2
 import os
 
+# Путь указываете свой!!
 BASE_DIR = r"C:\Users\Professional\PycharmProjects\TimiryazevCampusVision"
+
+# Шаг сохранения кадров (по умолчанию каждый 5)
+FRAME_STEP = 5
 
 
 def video_to_frames(video_name: str, campus_name: str):
-    """Извлекает кадры из видео и сохраняет их в папке, соответствующей выбранному корпусу."""
-
     video_path = os.path.join(BASE_DIR, "videos", f"{video_name}.mp4")
     output_path = os.path.join(BASE_DIR, "campuses", campus_name)
 
@@ -17,33 +19,46 @@ def video_to_frames(video_name: str, campus_name: str):
 
     print("\nПодождите, идет извлечение кадров из видео...")
     cap = cv2.VideoCapture(video_path)
+
     frames_count = 0
+    frame_index = 0
 
     while True:
         ret, frame = cap.read()
         if not ret:
             break
 
-        frame_filename = os.path.join(output_path, f"frame_{frames_count:04d}.jpg")
-        cv2.imwrite(frame_filename, frame)
-        frames_count += 1
+        if frame_index % FRAME_STEP == 0:
+            frame_filename = os.path.join(
+                output_path,
+                f"frame_{frames_count:04d}.jpg"
+            )
+            cv2.imwrite(frame_filename, frame)
+            frames_count += 1
+
+        frame_index += 1
 
     cap.release()
     print(f"Было извлечено кадров: {frames_count}")
-    return None
+    return frames_count
 
 
 def main():
     while True:
         print("\n1. Нарезать видео на кадры\n2. Выйти")
         choice = input(">>> ")
+
         if choice == "2":
             print("Выход.")
             break
+
         elif choice == "1":
             video_name = input("Введите название видео (без расширения): ")
             campus_name = input("Введите название корпуса: ")
-            video_to_frames(video_name, campus_name)
+            result = video_to_frames(video_name, campus_name)
+            if isinstance(result, str):
+                print(result)
+
         else:
             print("Некорректный выбор. Попробуйте снова.")
 
@@ -52,4 +67,4 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print("Досрочное завершение програмы")
+        print("Досрочное завершение программы")
